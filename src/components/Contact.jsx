d884da6c36b2880c1c43be5b2b3fbe742ef14b42
Jeff Wang <jeffwang20736@gmail.com>
2025-01-02 14:23:57 -0500

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
         'template_hpvh5jd', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'Allen Liu', // put your name here.
+          to_name: 'Fei L', // put your name here.
           from_email: form.email,
           to_email: 'portforlio@norepy.com', //put your email here.
           message: form.message,
